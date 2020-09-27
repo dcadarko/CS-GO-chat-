0dc42f69d0020370283e3d02636538acef6360ec
Darko Cvetkovski <darkodarko620@gmail.com>
2020-09-27 21:23:01 +0200

    CS:GO chat

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ document.getElementById("message").addEventListener("keydown", (e) => {
     e.preventDefault();
     setTimeout(() => {
       document.getElementById("sendbtn").click();
-    }, 400);
+    }, 500);
   }
 });
-
-window.onbeforeunload = function () {
-  return "Data will be lost if you leave the page, are you sure?";
-};
 
 document.getElementById("message").addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
@@ -37,9 +33,28 @@ var setusername = () => {
   socket.emit("username", document.getElementById("username").value);
 };
 
-socket.on("user", (user) => {
+var stats = [];
+
+socket.on("user", async (user) => {
   username = user;
   console.log(user);
+});
+
+socket.on("stats", (data) => {
+  var table = document.getElementById("users-table");
+  var row = table.insertRow(1);
+  var cell1 = row.insertCell(0);
+  var cell2 = row.insertCell(1);
+  var cell3 = row.insertCell(2);
+  var kd = data.kd;
+  cell1.classList.add(username);
+  cell2.classList.add(kd.toFixed(2));
+  cell3.classList.add(data.time.toFixed() + "h");
+  cell1.innerHTML = username;
+  cell2.innerHTML = kd.toFixed(2);
+  cell3.innerHTML = data.time.toFixed() + " h";
+  $(`.${username}`).not(":first").siblings().hide();
+  $(`.${username}`).not(":first").hide();
 });
 
 var username = "";
