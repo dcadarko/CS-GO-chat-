245a89e039ebb9deaaebe0290d6b59159652c843
Darko Cvetkovski <darkodarko620@gmail.com>
2020-10-01 20:57:22 +0200

    Added more code documentation

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var socket = io("https://cs-go-chat.herokuapp.com/");
 
+/* Random number of ms to prevent message mixing up */
 var randint = Math.floor(Math.random() * 350 + 200);
 
+/* Sending a message */
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
     const button = document.getElementById("sendbtn");
@@ -16,6 +18,7 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
+/* Old code that i won't remove, because the app is working as it should */
 document.body.addEventListener("keydown", (e) => {
   if (e.keyCode == 13) {
     e.preventDefault();
@@ -23,23 +26,27 @@ document.body.addEventListener("keydown", (e) => {
   }
 });
 
+/* Function for message sending */
 var send = () => {
   socket.emit("message", document.getElementById("message").value);
   document.getElementById("message").value = " ";
   return false;
 };
 
+/* Getting username */
 socket.on("user", async (user) => {
   username = user;
   console.log(user);
 });
 
+/* Sending username */
 var setusername = () => {
   socket.emit("username", document.getElementById("username").value);
 };
 
 var stats = [];
 
+/* Displaying stats */
 socket.on("stats", (data) => {
   var table = document.getElementById("users-table");
   var row = table.insertRow(1);
@@ -60,6 +67,7 @@ socket.on("stats", (data) => {
 var username = "";
 var message = " ";
 
+/* Displaying messages */
 socket.on("message", (msg) => {
   message = msg;
   var placehoder = document.getElementById("msg-placeholder");
@@ -76,6 +84,7 @@ socket.on("message", (msg) => {
   }
 });
 
+/* Auto Scroll */
 window.setInterval(function () {
   var messagebox = document.getElementById("messages");
   messagebox.scrollTop = messagebox.scrollHeight;
